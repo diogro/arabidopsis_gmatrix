abac1dbe922ab32afe2c13f64d95e0950b0db75d
Diogo Melo <diogro@gmail.com>
2014-05-06 12:21:02 +0100

    calculating residual matrix

--- readArabdopsisData.R
+++ readArabdopsisData.R
@@ -7,6 +7,7 @@ library(MCMCglmm)
 library(gridExtra)
 library(gtools)
 
+
 raw_arabi_data <- read.csv2("./raw_data.csv")
 arabi_data <- select(raw_arabi_data, ID, RIL, Block, Partner, HEIGHT, WEIGHT, SILIQUEN, NODEN)
 names(arabi_data) <- c("ID", "RIL", "block", "partner", "height", "weight", "silique", "branch")
@@ -23,8 +24,15 @@ ggplot(m_arabi_data, aes(x = value, color = partner)) +
 geom_histogram() + theme_classic() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
 facet_wrap(~variable, ncol = 4, scale = "free_x")
 
-mask_L = arabi_data$partner == "L"
 traits = c('weight', 'height', 'silique', 'branch')
+names_g = paste0(traits, rep(c('D', 'S'), each = 4))
+padRmatrix <- function(x) {
+    R = array(0, c(2*num_traits, 2*num_traits))
+    R[1:num_traits, 1:num_traits] = x[,1:num_traits]
+    R[(num_traits+1):(2*num_traits), (num_traits+1):(2*num_traits)] = x[,(num_traits+1):(2*num_traits)]
+    dimnames(R) = list(names_g, names_g)
+    return(R)
+}
 
 model_block_D = lmer(value ~ variable + (variable|block), data = filter(m_arabi_data, partner == 'L'))
 model_block_S = lmer(value ~ variable + (variable|block), data = filter(m_arabi_data, partner == 'NONE'))
@@ -34,10 +42,10 @@ m_arabi_data_std$value[m_arabi_data_std$partner == 'L'] = residuals(model_block_
 m_arabi_data_std$value[m_arabi_data_std$partner == 'NONE'] = residuals(model_block_S)
 arabi_data_std = dcast(m_arabi_data_std, partner+ID+RIL~variable)
 
-arabi_data_std$weight  <- scale(arabi_data_std$weight)
-arabi_data_std$silique <- scale(arabi_data_std$silique)
-arabi_data_std$branch  <- scale(arabi_data_std$branch)
-arabi_data_std$height  <- scale(arabi_data_std$height)
+
+mask_L = arabi_data_std$partner == "L"
+arabi_data_std[ mask_L, traits] <- scale(arabi_data_std[ mask_L, traits])
+arabi_data_std[!mask_L, traits] <- scale(arabi_data_std[!mask_L, traits])
 
 num_traits = 4
 prior = list(R = list(R1 = list(V = diag(num_traits), n = 0.002),
@@ -51,23 +59,23 @@ arabi_model = MCMCglmm(cbind(weight, height, silique, branch) ~ trait:partner - 
                        prior = prior,
                        data = arabi_data_std)
 Gs = array(arabi_model$VCV[,1:(4*num_traits*num_traits)], dim = c(1000, 2*num_traits, 2*num_traits))
+#Rs = array(arabi_model$VCV[,-(1:(4*num_traits*num_traits))], dim = c(1000, num_traits, 2*num_traits))
+Rs = aaply(array(arabi_model$VCV[,-(1:(4*num_traits*num_traits))], dim = c(1000, num_traits, 2*num_traits)), 1, padRmatrix)
 G_mcmc = apply(Gs, 2:3, mean)
+R_mcmc = apply(Rs, 2:3, mean)
 corr_G = cov2cor(G_mcmc)
-names_g = paste0(traits, rep(c('D', 'S'), each= 4))
-dimnames(G_mcmc)  = dimnames(corr_G) = list(names_g, names_g)
-#corr_G[5:8, 1:4] = cov2cor(G_mcmc[5:8, 1:4])
-#corr_G[1:4, 5:8] = cov2cor(G_mcmc[1:4, 5:8])
-diag(G_mcmc)
+corr_R = cov2cor(R_mcmc)
+dimnames(G_mcmc) = dimnames(R_mcmc) = dimnames(corr_R) = dimnames(corr_G) = list(names_g, names_g)
 G_mcmc_conf = apply(Gs, 2:3, quantile, c(0.025, 0.975))
 G_mcmc_conf = aperm(G_mcmc_conf, c(2, 3, 1))
 containsZero = function(x) ifelse(0 > x[1] & 0 < x[2], TRUE, FALSE)
 significant = !aaply(G_mcmc_conf, 1:2, containsZero)
-herit = data.frame(trait = traits,
-                   partner = rep(c('D', 'S'), each= 4),
+herit = data.frame(trait = factor(traits, levels = traits),
+                   partner = factor(rep(c('D', 'S'), each = 4), levels = c('S', 'D')),
                    value = diag(G_mcmc),
                    upper = diag(G_mcmc_conf[,,2]),
                    lower = diag(G_mcmc_conf[,,1]))
 
 ggplot(herit, aes(trait, value, color = partner)) +
 geom_point() + geom_errorbar(aes(ymin=lower, ymax = upper)) +
-theme_classic() + labs(y = 'heritabilities', x = 'trait') + facet_wrap(~partner)
+theme_classic(base_size = 20) + labs(y = 'heritabilities', x = 'trait') + facet_wrap(~partner)
