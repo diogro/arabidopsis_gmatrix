b88c6db1dd03d44ed236182128944e1c0d66c039
Diogo Melo <diogro@gmail.com>
2014-05-20 17:29:22 +0100

    block effects, lognormal silique

--- runStanModel.R
+++ runStanModel.R
@@ -15,9 +15,9 @@ arabi_data$flower[is.na(arabi_data$flower)] <- 0
 arabi_data = arabi_data[complete.cases(arabi_data),]
 #arabi_data = arabi_data[arabi_data$flower > 0,]
 
-arabi_data$weight  <- sqrt(arabi_data$weight)
-arabi_data$silique  <- sqrt(arabi_data$silique)
-arabi_data$iszero_silique = arabi_data$silique == 0
+arabi_data$weight  <- log(arabi_data$weight)
+mask_0 = arabi_data$iszero_silique = arabi_data$silique == 0
+arabi_data$silique[!mask_0]  <- log(arabi_data$silique[!mask_0] + 1)
 #arabi_data$silique[arabi_data$silique == 0] = NA
 plot(silique~weight, arabi_data)
 plot(silique~height, arabi_data)
@@ -28,75 +28,97 @@ ggplot(m_arabi_data, aes(x = value, color = partner)) +
 geom_histogram() + theme_classic() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
 facet_wrap(~variable, ncol = 5, scale = "free")
 
-lme4_model = glmer(branch ~ partner + (partner|RIL),
+lme4_model = glmer(branch ~ partner + (partner|RIL) + (1|block),
                    family ="poisson", data = arabi_data)
 summary(lme4_model)
 
 traits = c('weight', 'height', 'silique', 'branch')
 names_g = paste0(traits, rep(c('D', 'S'), each = 4))
 
+branch_model = glmer2stan(branch ~ partner + (partner|RIL) + (1|block),
+                          data=arabi_data,
+                          family="poisson"
+                          sample = FALSE, calcDIC = FALSE)
+write(silique_model$model, file = "branch.stan")
+
 branch_data <- list(N = dim(arabi_data)[1],
                     branch = arabi_data$branch,
                     partnerNONE = as.integer(as.factor(arabi_data$partner)),
                     RIL = as.integer(as.factor(arabi_data$RIL)),
                     N_RIL = length(unique(arabi_data$RIL)))
-stan_model = stan(file = './branch.stan', data = branch_data, chain=1)
-stan_samples = extract(stan_model, permuted = TRUE)
+branch_stan_model = stan(file = './branch.stan', data = branch_data, chain=1)
+bm = extract(branch_stan_model, permuted = TRUE)
 
 
-the_formula <- list(iszero_silique ~ partner + (partner|RIL) ,
-                    silique        ~ partner + (partner|RIL))
+the_formula <- list(iszero_silique ~ partner + (partner|RIL) + block,
+                    silique        ~ partner + (partner|RIL) + block)
 silique_model = glmer2stan(the_formula, data=arabi_data,
                            family=list("binomial","gamma"),
                            sample = FALSE, calcDIC = FALSE)
 write(silique_model$model, file = "silique.stan")
 
-
 N_pi              = dim(arabi_data)[1]
-N_mu              = sum(arabi_data$silique!=0)
+N_mu              = sum(arabi_data$silique!= 0)
 iszero_silique_pi = as.integer(arabi_data$iszero_silique)
-partnerNONE_pi    = as.integer(as.factor(arabi_data$partner))
+partnerNONE_pi    = as.integer(as.factor(arabi_data$partner)) - 1
 RIL_pi            = as.integer(as.factor(arabi_data$RIL))
+block_pi          = as.integer(as.factor(arabi_data$block)) - 1
 bin_total_pi      = rep(1,dim(arabi_data)[1])
-silique_mu        = arabi_data$silique[!arabi_data$iszero_silique]
-partnerNONE_mu    = as.integer(as.factor(arabi_data$partner))[!arabi_data$iszero_silique]
-RIL_mu            = as.integer(as.factor(arabi_data$RIL))[!arabi_data$iszero_silique]
+silique_mu        = arabi_data$silique[!mask_0]
+partnerNONE_mu    = partnerNONE_pi[!mask_0]
+RIL_mu            = as.integer(as.factor(arabi_data$RIL))[!mask_0]
+block_mu          = block_pi[!mask_0] - 1
 N_RIL             = length(unique(arabi_data$RIL))
+silique_data <- list(N_pi              = N_pi,
+                     N_mu              = N_mu,
+                     iszero_silique_pi = iszero_silique_pi,
+                     partnerNONE_pi    = partnerNONE_pi,
+                     RIL_pi            = RIL_pi,
+                     block_pi          = block_pi,
+                     bin_total_pi      = bin_total_pi,
+                     silique_mu        = silique_mu,
+                     partnerNONE_mu    = partnerNONE_mu,
+                     RIL_mu            = RIL_mu,
+                     block_mu          = block_mu,
+                     N_RIL             = N_RIL,
+                     N_block           = N_block)
+silique_stan_model = stan(file = './silique.stan', data = silique_data, chain=1)
 
-silique_data <- list(N_pi              = dim(arabi_data)[1],
-                     N_mu              = sum(arabi_data$silique!=0),
-                     iszero_silique_pi = as.integer(arabi_data$iszero_silique),
-                     partnerNONE_pi    = as.integer(as.factor(arabi_data$partner)),
-                     RIL_pi            = as.integer(as.factor(arabi_data$RIL)),
-                     bin_total_pi      = rep(1,dim(arabi_data)[1]),
-                     silique_mu        = arabi_data$silique[!arabi_data$iszero_silique],
-                     partnerNONE_mu    = as.integer(as.factor(arabi_data$partner))[!arabi_data$iszero_silique],
-                     RIL_mu            = as.integer(as.factor(arabi_data$RIL))[!arabi_data$iszero_silique],
-                     N_RIL             = length(unique(arabi_data$RIL)))
-stan_model = stan(file = './silique.stan', data = silique_data, chain=1)
-stan_samples = extract(stan_model, permuted = TRUE)
-attach(stan_samples)
-replicates = dim(vary_RIL)[1]
-bin_sim = array(0, c(replicates, N_pi))
+sm = extract(silique_stan_model, permuted = TRUE)
+replicates = dim(sm$vary_RIL)[1]
+bin_sim = array(0, c(replicates, sm$N_pi))
 for(i in 1:replicates){
-    vary_pi <- vary_RIL[i, RIL_pi,1] + vary_RIL[i, RIL_pi,2] * partnerNONE_pi
-    glm_pi <- vary_pi + Intercept_pi[i] + beta_partnerNONE_pi[i] * partnerNONE_pi
+    vary_pi <- sm$vary_RIL[i, RIL_pi,1] + sm$vary_RIL[i, RIL_pi,2] * sm$partnerNONE_pi
+    glm_pi <- vary_pi + sm$Intercept_pi[i] +
+              sm$beta_partnerNONE_pi[i] * sm$partnerNONE_pi +
+              sm$beta_block_pi[i] * sm$block_pi
     glm_pi <- inv.logit( glm_pi )
     for(j in 1:N_pi)
         bin_sim[i,j] = rbinom(1, 1, prob = glm_pi[j])
 }
 hist(apply(bin_sim, 1, sum)); abline(v = sum(arabi_data$iszero_silique), col = "red")
 post_zeros = mean(apply(bin_sim, 1, sum))
-
-silique_sim = array(0, c(replicates, N_mu))
+silique_sim = array(0, c(replicates, sm$N_mu))
 for(i in 1:replicates){
-    vary_mu <- vary_RIL[i, RIL_mu, 3] + vary_RIL[i, RIL_mu,4] * partnerNONE_mu
-    glm_mu <- vary_mu + Intercept_mu[i] + beta_partnerNONE_mu[i] * partnerNONE_mu
-    glm_mu <- exp( glm_mu )*theta_mu[i];
+    vary_mu <- sm$vary_RIL[i, RIL_mu, 3] + sm$vary_RIL[i, RIL_mu,4] * sm$partnerNONE_mu
+    glm_mu <- vary_mu + sm$Intercept_mu[i] +
+              sm$beta_partnerNONE_mu[i] * sm$partnerNONE_mu +
+              sm$beta_block_mu[i] * sm$block_mu
+    glm_mu <- exp(glm_mu + 0.5 * sm$sigma[i] * sm$sigma[i])
     for ( j in 1:N_mu ) {
-        silique_sim[i,j] = rgamma(1, glm_mu[j] , theta_mu[i])
+        silique_sim[i,j] = rlnorm(1, glm_mu[j], sm$sigma[i])
     }
 }
-par(mfrow=c(1, 2))
-hist((apply(silique_sim, 2, mean))^2, xlim=c(0, 150))
-hist(arabi_data$silique[!arabi_data$iszero_silique]^2, xlim=c(0, 150))
+par(mfrow=c(2, 2))
+hist((apply(silique_sim, 1, min))) ; abline(v =  min(((arabi_data$silique[!mask_0]))), col  = "red")
+hist((apply(silique_sim, 1, mean))); abline(v = mean(((arabi_data$silique[!mask_0]))), col = "red")
+hist((apply(silique_sim, 1, max))) ; abline(v =  max(((arabi_data$silique[!mask_0]))), col  = "red")
+hist((apply(silique_sim, 1, sum))) ; abline(v =  sum(((arabi_data$silique[!mask_0]))), col  = "red")
+
+hist(sm$sigma); abline(v = sd(log(arabi_data$silique[!mask_0])), col = "red")
+
+mean(silique_sim[2,])
+mean(log(arabi_data$silique[!mask_0]))
+
+qqplot(silique_sim[2,], log(arabi_data$silique[!mask_0]))
+#qqline(silique_sim[1,], log(arabi_data$silique[!mask_0]))
