563df29032497cf49910a443be9b1b79cb4e9c60
diogro <diogro@gmail.com>
2014-06-17 14:27:43 -0300

    very simple models

--- runlme4Models.R
+++ runlme4Models.R
@@ -48,6 +48,15 @@ arabi_data$weight  = sqrt(arabi_data$weight)
 arabi_data$silique = sqrt(arabi_data$silique)
 arabi_data$branch  = sqrt(arabi_data$branch)
 
+#################################################################
+# Removing RIL with no replication in both environments
+#################################################################
+
+mask = apply(table(arabi_data$RIL, arabi_data$partner), 1, function(x) all(x > 1))[arabi_data$RIL]
+arabi_data = arabi_data[mask,]
+arabi_data$RIL = as.character(arabi_data$RIL)
+table(arabi_data$RIL, arabi_data$partner)
+
 ##########################################
 # Scaling for heritability estimation
 ##########################################
@@ -85,28 +94,11 @@ facet_wrap(~variable, ncol = 5, scale = "free")
 m_arabi_data = melt(select(arabi_data, ID, block, RIL, partner, weight_std, height_std, silique_std), id.vars = c('partner', 'block', 'ID', 'RIL'))
 #m_arabi_data = melt(select(arabi_data, ID, block, RIL, partner, weight, height, silique), id.vars = c('partner', 'block', 'ID', 'RIL'))
 multi_model = lmer(value ~ variable + variable:partner + variable:block + (0 + variable:partner|RIL),
-                   data = m_arabi_data, REML = FALSE, na.action = 'na.omit', control=lmerControl(check.conv.singular="warning"))
+                   data = m_arabi_data, na.action = 'na.omit', control=lmerControl(check.conv.singular="warning"))
 summary(multi_model)
 VarCorr(multi_model)
 VarCorr(multi_model)$RIL
 diag(VarCorr(multi_model)$RIL)
-
-table(arabi_data$RIL, arabi_data$partner)
-
-#############################
-# Multivariate Linear Model
-#############################
-
-m_arabi_data = melt(select(arabi_data, ID, block, RIL, partner, weight_std, height_std, silique_std), id.vars = c('partner', 'block', 'ID', 'RIL'))
-#m_arabi_data = melt(select(arabi_data, ID, block, RIL, partner, weight, height, silique), id.vars = c('partner', 'block', 'ID', 'RIL'))
-multi_model = lm(value ~ variable*partner*block*RIL,
-                 data = m_arabi_data, REML = FALSE, na.action = 'na.omit',)
-summary(multi_model)
-VarCorr(multi_model)
-VarCorr(multi_model)$RIL
-diag(VarCorr(multi_model)$RIL)
-
-table(arabi_data$RIL, arabi_data$partner)
 
 ##########################
 # Univariate Models
@@ -117,7 +109,7 @@ table(arabi_data$RIL, arabi_data$partner)
 ###################
 
 # un-scaled
-silique_model = lmer(silique ~ 1 + partner + block + (0 + partner|RIL),
+silique_model = lmer(silique ~ partner + (0 + partner|RIL),
                     data = arabi_data, na.action = 'na.omit')
 summary(silique_model)
 varRIL = diag(VarCorr(silique_model)$RIL)
@@ -125,18 +117,18 @@ varRes = rep(attributes(VarCorr(silique_model))$sc^2, 2)
 (h2 = varRIL/(varRIL + varRes))
 
 # scaled
-silique_model = lmer(silique_std ~ 1 + partner + block + (0 + partner|RIL),
+silique_model = lmer(silique_std ~ partner + (0 + partner|RIL),
                      data = arabi_data, na.action = 'na.omit')
 summary(silique_model)
 varRIL = diag(VarCorr(silique_model)$RIL)
 varRes = rep(attributes(VarCorr(silique_model))$sc^2, 2)
-(h2 = varRIL/(varRIL + varRes))
+(h2 = varRIL)
 
 # scaled per treatment
-silique_model_D = lmer(silique_std ~ 1 + block + (1|RIL),
+silique_model_D = lmer(silique_std ~ (1|RIL),
                        data = filter(arabi_data, partner == "D"))
 summary(silique_model_D)
-silique_model_S = lmer(silique_std ~ 1 + block + (1|RIL),
+silique_model_S = lmer(silique_std ~ (1|RIL),
                        data = filter(arabi_data, partner == "S"))
 summary(silique_model_S)
 varRIL = c("D" = VarCorr(silique_model_D)$RIL, "S" = VarCorr(silique_model_S)$RIL)
@@ -147,25 +139,25 @@ varRIL = c("D" = VarCorr(silique_model_D)$RIL, "S" = VarCorr(silique_model_S)$RI
 ###################
 
 # un-scaled
-weight_model = lmer(weight ~ 1 + partner + block + (0 + partner|RIL),
-                    data = arabi_data, REML = FALSE, na.action = 'na.omit')
+weight_model = lmer(weight ~ partner + (0 + partner|RIL),
+                    data = arabi_data, na.action = 'na.omit')
 summary(weight_model)
 varRIL = diag(VarCorr(weight_model)$RIL)
 varRes = rep(attributes(VarCorr(weight_model))$sc^2, 2)
 (h2 = varRIL/(varRIL + varRes))
 
 # scaled
-weight_model = lmer(weight_std ~ 1 + partner + block + (0 + partner|RIL),
-                    data = arabi_data, REML = FALSE, na.action = 'na.omit')
+weight_model = lmer(weight_std ~ partner + (0 + partner|RIL),
+                    data = arabi_data, na.action = 'na.omit')
 summary(weight_model)
 varRIL = diag(VarCorr(weight_model)$RIL)
 (h2 = varRIL)
 
 # scaled per treatment
-weight_model_D = lmer(weight_std ~ 1 + block + (1|RIL),
+weight_model_D = lmer(weight_std ~ (1|RIL),
                        data = filter(arabi_data, partner == "D"))
 summary(weight_model_D)
-weight_model_S = lmer(weight_std ~ 1 + block + (1|RIL),
+weight_model_S = lmer(weight_std ~ (1|RIL),
                        data = filter(arabi_data, partner == "S"))
 summary(weight_model_S)
 varRIL = c("D" = VarCorr(weight_model_D)$RIL, "S" = VarCorr(weight_model_S)$RIL)
@@ -176,26 +168,26 @@ varRIL = c("D" = VarCorr(weight_model_D)$RIL, "S" = VarCorr(weight_model_S)$RIL)
 ###################
 
 # un-scaled
-height_model = lmer(height ~ 1 + partner + block + (0 + partner|RIL),
-                    data = arabi_data, REML = FALSE, na.action = 'na.omit')
+height_model = lmer(height ~ partner + (0 + partner|RIL),
+                    data = arabi_data, na.action = 'na.omit')
 summary(height_model)
 varRIL = diag(VarCorr(height_model)$RIL)
 varRes = rep(attributes(VarCorr(height_model))$sc^2, 2)
 (h2 = varRIL/(varRIL + varRes))
 
 # scaled
-height_model = lmer(height_std ~ 1 + partner + block + (0 + partner|RIL),
-                    data = arabi_data, REML = FALSE, na.action = 'na.omit')
+height_model = lmer(height_std ~ partner + (0 + partner|RIL),
+                    data = arabi_data, na.action = 'na.omit')
 summary(height_model)
 varRIL = diag(VarCorr(height_model)$RIL)
 varRes = rep(attributes(VarCorr(height_model))$sc^2, 2)
 (h2 = varRIL)
 
 # scaled per treatment
-height_model_D = lmer(height_std ~ 1 + block + (1|RIL),
+height_model_D = lmer(height_std ~ (1|RIL),
                        data = filter(arabi_data, partner == "D"))
 summary(height_model_D)
-height_model_S = lmer(height_std ~ 1 + block + (1|RIL),
+height_model_S = lmer(height_std ~ (1|RIL),
                        data = filter(arabi_data, partner == "S"))
 summary(height_model_S)
 varRIL = c("D" = VarCorr(height_model_D)$RIL, "S" = VarCorr(height_model_S)$RIL)
